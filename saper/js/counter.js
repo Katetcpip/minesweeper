bfa02eb4a8581dec0fa18662c00160f41c710578
Katetcpip <90146349+Katetcpip@users.noreply.github.com>
2023-03-03 22:26:26 +0100

    Add files via upload

---
+++ saper/js/counter.js
@@ -0,0 +1,11 @@
+var Counter = function(x, y, defaultValue) {
+
+    var currentValue = defaultValue;
+    var tf_counter = game.add.text(x, y, 'Мины: ' + defaultValue, fontStyles.counterFontStyle);
+
+    tf_counter.anchor.set(1, 0.5);
+
+    this.update = function(value) {
+        tf_counter.text = 'Мины: ' + (currentValue - value);
+    };
+};
